e962caf7f93a0505b463742e86a3171260205a98
Ghastlymicrowave <38994952+Ghastlymicrowave@users.noreply.github.com>
2021-05-25 21:55:11 -0400

    2d movement

--- main/LonelyChild DuckNoir/Assets/Scripts/player_main.cs
+++ main/LonelyChild DuckNoir/Assets/Scripts/player_main.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class player_main : MonoBehaviour
 {
-    [Range(.01f,0.5f)]public float maxSpd = 2f;
-    [Range(.001f,0.1f)]public float initalSpd = .1f;
+    [Range(.1f,10f)]public float maxSpd = 2f;
+    [Range(.1f,10f)]public float initalSpd = .1f;
     [Range(0.1f,5f)]public float secToMax = 2f;//seconds it takes to reach max spd from itial speed
     [Range(0f,1f)]public float Kfriction =.2f;//in percent of speed
-    [Range(0f,0.1f)]public float Sfriction = .5f;//in units/s
+    [Range(0f,5f)]public float Sfriction = .5f;//in units/s
 
 
     float currentSpd = 0;
-    private float spdAccel {get {return (maxSpd-initalSpd / secToMax);}}
+    public float spdAccel;
     Vector2 facing = Vector2.zero;
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
@@ -31,8 +33,13 @@ public class player_main : MonoBehaviour
                 currentSpd = initalSpd;
                 facing = new Vector2(hinput,vinput).normalized;
             }else{
-                currentSpd = Mathf.Min(spdAccel+currentSpd,maxSpd);
-                facing = Vector2.Lerp(facing,new Vector2(hinput,vinput).normalized,0.6f).normalized;//rotate angle with lerp
+                currentSpd = Mathf.Min(spdAccel*Time.deltaTime+currentSpd,maxSpd);
+                if (Vector2.Angle(new Vector2(hinput,vinput),facing)>160f){
+                    facing = Vector2.Lerp(facing,new Vector2(hinput,vinput).normalized,0.51f).normalized;
+                }else{
+                    facing = Vector2.Lerp(facing,new Vector2(hinput,vinput).normalized,0.05f).normalized;//rotate angle with lerp
+                }
+                
             }
         }else{//not actively moving, apply friction
             currentSpd = Mathf.Max(0f,currentSpd-currentSpd*Kfriction);
@@ -42,7 +49,9 @@ public class player_main : MonoBehaviour
         }
 
         if (currentSpd!=0){
-            transform.position+= new Vector3(facing.x,0f,facing.y)*currentSpd;
+            rb.velocity = new Vector2(facing.x,facing.y)*currentSpd;
+        }else{
+            rb.velocity = Vector2.zero;
         }
     }
 }
