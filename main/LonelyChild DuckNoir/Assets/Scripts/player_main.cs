c1c896dfa065cfe7a5ef8fee965ed7bc5bc991f7
Ghastlymicrowave <38994952+Ghastlymicrowave@users.noreply.github.com>
2021-05-25 21:02:25 -0400

    Basic movement

--- main/LonelyChild DuckNoir/Assets/Scripts/player_main.cs
+++ main/LonelyChild DuckNoir/Assets/Scripts/player_main.cs
@@ -4,20 +4,19 @@ using UnityEngine;
 
 public class player_main : MonoBehaviour
 {
-    [Range(.01f,10f)]public float maxSpd = 2f;
-    [Range(.001f,5f)]public float initalSpd = .1f;
-    [Range(0.1f,10f)]public float secToMax = 2f;//seconds it takes to reach max spd from itial speed
+    [Range(.01f,0.5f)]public float maxSpd = 2f;
+    [Range(.001f,0.1f)]public float initalSpd = .1f;
+    [Range(0.1f,5f)]public float secToMax = 2f;//seconds it takes to reach max spd from itial speed
     [Range(0f,1f)]public float Kfriction =.2f;//in percent of speed
-    [Range(0f,1f)]public float Sfriction = .5f;//in units/s
+    [Range(0f,0.1f)]public float Sfriction = .5f;//in units/s
 
 
     float currentSpd = 0;
-    float spdAccel;
+    private float spdAccel {get {return (maxSpd-initalSpd / secToMax);}}
     Vector2 facing = Vector2.zero;
     // Start is called before the first frame update
     void Start()
     {
-        spdAccel = (maxSpd-initalSpd / secToMax);
     }
 
     // Update is called once per frame
@@ -28,12 +27,12 @@ public class player_main : MonoBehaviour
         bool isMoving = (hinput!=0f || vinput!=0f);
         
         if (isMoving){
-            if (currentSpd==0){//moving from standstill
+            if (currentSpd<initalSpd){//moving from standstill
                 currentSpd = initalSpd;
                 facing = new Vector2(hinput,vinput).normalized;
             }else{
                 currentSpd = Mathf.Min(spdAccel+currentSpd,maxSpd);
-                facing = Vector2.Lerp(facing,new Vector2(hinput,vinput).normalized,0.4f).normalized;//rotate angle with lerp
+                facing = Vector2.Lerp(facing,new Vector2(hinput,vinput).normalized,0.6f).normalized;//rotate angle with lerp
             }
         }else{//not actively moving, apply friction
             currentSpd = Mathf.Max(0f,currentSpd-currentSpd*Kfriction);
