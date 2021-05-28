88a9de67838ca1b9aaa9cdbc6a4bd94345880050
Zkadera <ducknoirgames@gmail.com>
2021-05-28 01:15:20 -0400

    New combat changes

--- main/LonelyChild DuckNoir/Assets/Scripts/Combat/battleButton.cs
+++ main/LonelyChild DuckNoir/Assets/Scripts/Combat/battleButton.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Combat;
 
 public class battleButton : MonoBehaviour
 {
     //Call buttonpress on battlebehavior when clicked.
     public battleBehavior bb;
-    public int buttonNum;
+    public ButtonEnum buttonNum;
+    public SpriteRenderer sr;
+    void OnMouseOver()
+    {
+        sr.color = Color.grey;
+    }
+    void OnMouseExit()
+    {
+        sr.color = Color.white;
+    }
     void OnMouseDown()
     {
         bb.ButtonPress(buttonNum);
