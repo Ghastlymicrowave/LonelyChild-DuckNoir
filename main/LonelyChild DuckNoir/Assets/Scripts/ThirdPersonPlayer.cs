c8c380ef6f22c8b983a25f648751fe1f2be916cb
GhastlyMicrowave <supervaughanbros@gmail.com>
2021-06-27 23:46:35 -0400

    prefab

--- main/LonelyChild DuckNoir/Assets/Scripts/ThirdPersonPlayer.cs
+++ main/LonelyChild DuckNoir/Assets/Scripts/ThirdPersonPlayer.cs
@@ -53,9 +53,12 @@ public class ThirdPersonPlayer : MonoBehaviour
     Quaternion spotlightCurrent;
     Quaternion spotlightLerp;
     [SerializeField] Transform spotlight;
+    [SerializeField] Transform camLight;
+    bool UseCamLight = false;
     
     void Start()
     {
+        if (UseCamLight)
         thisAnimator = GetComponent<Animator>();
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
@@ -87,6 +90,9 @@ public class ThirdPersonPlayer : MonoBehaviour
         CameraSmooth = Mathf.Clamp(1-Mathf.Pow(inputArray[6],2),0.001f,1f);
         //.75 - 1*.74
         //.75 - 0
+
+        camLight.gameObject.SetActive(UseCamLight);
+        spotlight.gameObject.SetActive(!UseCamLight);
     }
 
     public bool ValidRequiresItem(){
@@ -188,8 +194,15 @@ public class ThirdPersonPlayer : MonoBehaviour
             }else{
                 currentSpeed = Mathf.Min(spdAccel + currentSpeed, maxSpd);
             }
-            thisAnimator.SetFloat("DirectionX",hinput);
-            thisAnimator.SetFloat("DirectionY",vinput);
+
+            if (UseCamLight){
+                thisAnimator.SetFloat("DirectionX",0f);
+                thisAnimator.SetFloat("DirectionY",1f);
+            }else{
+                thisAnimator.SetFloat("DirectionX",hinput);
+                thisAnimator.SetFloat("DirectionY",vinput);
+            }
+            
             spotlightLerp = Quaternion.LookRotation(new Vector3(direction.x,direction.y,0f),Vector3.back);
         }else{
             currentSpeed = Mathf.Max(0f,currentSpeed - currentSpeed * Kfriction);
@@ -217,8 +230,10 @@ public class ThirdPersonPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        spotlightCurrent = Quaternion.Lerp(spotlightCurrent,spotlightLerp,0.2f);
-        spotlight.localRotation = spotlightCurrent;
+        if (!UseCamLight){
+            spotlightCurrent = Quaternion.Lerp(spotlightCurrent,spotlightLerp,0.2f);
+            spotlight.localRotation = spotlightCurrent;
+        }
         if (canMove&&!InventoryOpen)
         {
             Look();
