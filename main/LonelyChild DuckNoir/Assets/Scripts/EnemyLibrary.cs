81ea27035b6c44ffe3537b8eeb44ea6a118dd25f
Zkadera <84819000+Zkadera@users.noreply.github.com>
2021-07-19 17:32:05 -0400

    Trouble Child Spin 1
    Gonna start pushng attacks 1 at a time in case more bullshit happens

--- main/LonelyChild DuckNoir/Assets/Scripts/EnemyLibrary.cs
+++ main/LonelyChild DuckNoir/Assets/Scripts/EnemyLibrary.cs
@@ -410,9 +410,13 @@ public class TroubledChild : EnemyClass
         canRun = false;
         //spritepath
         attackPrefabNames = new string[] {
-            "Prefabs/combatEnemyTurn/attacks/Sine_Harder",
-            "Prefabs/combatEnemyTurn/attacks/Straight_Wide_Easy_3",
-            "Prefabs/combatEnemyTurn/attacks/Straight_Wide_Easy",
+            "Prefabs/combatEnemyTurn/attacks/Spin_TC_1",
+            "Prefabs/combatEnemyTurn/attacks/Spin_TC_1",
+            "Prefabs/combatEnemyTurn/attacks/Spin_TC_1",
+            "Prefabs/combatEnemyTurn/attacks/Spin_TC_1"
+           // "Prefabs/combatEnemyTurn/attacks/Sine_Harder",
+           // "Prefabs/combatEnemyTurn/attacks/Straight_Wide_Easy_3",
+           // "Prefabs/combatEnemyTurn/attacks/Straight_Wide_Easy",
             };
         talkActions = new TalkEnum[][]{ 
             new TalkEnum[]{TalkEnum.Talk},
